2a76ec6520d447dfee364aa46fd99491a422cd77
katherine hu <hu.z.katherine@gmail.com>
2021-01-09 12:46:36 -0500

    webpack installed and working, eslint working

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    context: __dirname,
+    entry: {
+        bundle: ['./src/content.ts'],
+        background: ['./src/background.ts']
+    },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: '[name].js',
+        publicPath: '/public/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: '/node_modules/',
+                use: {
+                    loader: 'ts-loader'
+                }
+                //include: [path.resolve(__dirname, 'src')]
+            }
+        ]
+    },
+    resolve: {
+        extensions:['.ts', '.js']
+    }
+}
+
