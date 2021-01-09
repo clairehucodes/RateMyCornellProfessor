c8b01b4a4dc73b9a494cd3eb62e9aa7c92fd2846
[clairehucodes] <[hu.z.claire@gmail.com]>
2021-01-09 16:13:29 -0500

    changes

--- src/content.ts
+++ src/content.ts
@@ -15,7 +15,7 @@ const RED: string = '#FC4433';
 const LOADING_INDICATOR: string = '<img src="https://i.pinimg.com/originals/a6/8f/b5/a68fb58aa1ace26b0008f5a5dbcebfd2.jpg">';
 // The divs that contain possible locations for professor names to populate
 const $COURSE_LIST_AREAS: HTMLElement[] = [
-  document.getElementById('class-subject-listing'),
+  document.getElementById('class-subject-listing')
 ];
 
 // @ts-ignore
