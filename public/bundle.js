e0d453eb49ff025544d438cb745732c97633f324
[clairehucodes] <[hu.z.claire@gmail.com]>
2021-01-10 12:20:39 -0500

    no change?

--- public/bundle.js
+++ public/bundle.js
@@ -60,19 +60,22 @@ var RED = '#FC4433';
 var LOADING_INDICATOR = '<img src="https://i.pinimg.com/originals/a6/8f/b5/a68fb58aa1ace26b0008f5a5dbcebfd2.jpg">';
 // The divs that contain possible locations for professor names to populate
 var $COURSE_LIST_AREAS = [
-    document.getElementsByClassName('class-listing'),
+    document.getElementById('class-listing'),
 ];
 // @ts-ignore
 // chrome.runtime.sendMessage({ action: 'showIcon' });
 // Watch each of the areas where professor names may appear for changes. When detected, rate each professor.
 var getOverallScoresObserver;
-if ($COURSE_LIST_AREAS[1] != null) {
+if ($COURSE_LIST_AREAS.item[0] != null) {
     getOverallScoresObserver = new MutationObserver(rateProfessorsOnPage);
     console.log($COURSE_LIST_AREAS);
     $COURSE_LIST_AREAS.forEach(function (area) { return getOverallScoresObserver.observe(area, { childList: true }); });
 }
 else {
-    console.log("$COURSE_LIST_AREAS[1] EQUALS null");
+    // getOverallScoresObserver = new MutationObserver(rateProfessorsOnPage);
+    // console.log($COURSE_LIST_AREAS);
+    // $COURSE_LIST_AREAS.forEach(area => getOverallScoresObserver.observe(area, { childList: true }));
+    console.log("$COURSE_LIST_AREAS EQUALS null");
 }
 /**
  * Rates each of the professors currently in view.
@@ -116,7 +119,8 @@ function rateProfessorsOnPage() {
  */
 function getProfessorNodes() {
     var returnVal;
-    returnVal = document.getElementById('tooltip-iws')[0];
+    returnVal = document.getElementById('instructors').getAttribute('data-content');
+    returnVal.forEach(function (item) { return console.log(item); });
     if (returnVal != null) {
         returnVal = returnVal.getAttribute('data-content');
     }
